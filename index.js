e9b3fa0455470ae49d03a81e5c6e752f69e26ed4
Conteporchetta <giulia.zobeide@gmail.com>
2025-06-03 14:24:45 +0200

    Test http

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log('Hello, World!');
+
+// const http = require('http');
+// http.createServer(function(req, res) {
+//     res.writeHead(200,
+//         {'Content-Type': 'text/plain'});
+//     res.write('Hello, World!!!\n');
+//     res.end();
+// }).listen(8000);
+
+const http = require('http');
+http.createServer(function(req, res) {
+    if (req.url === '/books') {
+    res.writeHead(404,
+        {'Content-Type': 'text/plain'});
+    res.write('Non ci sono libri qui!\n');
+    res.end();
+    } else {
+    res.writeHead(200,
+        {'Content-Type': 'text/plain'});
+    res.write('Hello, World!!!\n');
+    res.end();
+    }
+}).listen(8080);
