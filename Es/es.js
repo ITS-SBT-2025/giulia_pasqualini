0722802c6f27dee018d85918e816f22ed825be25
Conteporchetta <giulia.zobeide@gmail.com>
2025-06-19 16:46:37 +0200

    Update es

--- Es/es.js
+++ Es/es.js
@@ -1,42 +1,26 @@
-//Import
 import "dotenv/config";
 import express from 'express';
 import pageRoute from './Components/Pages/pages.route.js';
 import productRoute from './Components/Products/products.route.js'
 import userRoute from './Components/Users/users.route.js';
+import authRoutes from "./Components/Authentication/authentication.route.js";
+import errorMiddleware from "./Middleware/error.middleware.js";
+import verifyToken from "./Middleware/verify-token.middleware.js";
 
 const app = express()
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(req, res, next => {
-//     console.log("Questo è un middleware");
-//     next();
-// });
-// app.use(req, res, next => {
-//     console.log("Anche questo è un middleware");
-//     next();
-// });
-
 app.use(pageRoute);
+app.use("/users", verifyToken, usersRoutes);
 app.use("/products", productRoute);
-app.use("/users", userRoute);
+app.use("/auth", authRoutes);
+app.use(errorMiddleware);
 
 app.use((req, res) => { 
     res.status(404).send("Pagina non trovata.");
 });
-
-app.use((err, req, res, next) => {
-  if (err.status >= 400 && err.status <= 499) {
-    res.status(err.status).json({ error: err.message });
-  } else {
-    console.error(err.stack);
-
-    res.status(500).json({ error: "Qualcosa è andato storto!" });
-  }
-});
-
 
 //Server
 app.listen(process.env.PORT, () => {
@@ -60,5 +44,14 @@ app.listen(process.env.PORT, () => {
 //   } else {
 //     res.writeHead(404, {"Content-Type": "text/plain"});
 //     res.end("Pagina non trovata.");
+//   }
+// });
+// app.use((err, req, res, next) => {
+//   if (err.status >= 400 && err.status <= 499) {
+//     res.status(err.status).json({ error: err.message });
+//   } else {
+//     console.error(err.stack);
+
+//     res.status(500).json({ error: "Qualcosa è andato storto!" });
 //   }
 // });
